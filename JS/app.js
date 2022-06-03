7d8906b2e1ce2636cb889517d50f413102fd77a0
Bruno Guimarães <devgmrs@gmail.com>
2022-06-03 13:13:25 -0300

    :bug: Corrigindo o bug que deixa a tecla ativa ao usar o TAB

--- JS/app.js
+++ JS/app.js
@@ -4,10 +4,23 @@ const setIdAudio = (idAudio) => {
 	document.querySelector(`#som_${idAudio}`).play();
 };
 
+const preencherCorTecla = (atributo, classCss) => {
+	atributo.onkeydown = (event) => {
+		if (event.code === 'Enter' || event.code === 'Space') {
+			atributo.classList.add(classCss);
+		}
+	};
+
+	atributo.onkeyup = () => {
+		atributo.classList.remove(classCss);
+	};
+};
+
 const tecladoMusical = () => {
 	for (const teclasDoTeclado of teclado) {
 		const idDoAudio = teclasDoTeclado.classList[1];
 		teclasDoTeclado.addEventListener('click', () => {
+			preencherCorTecla(teclasDoTeclado, 'test');
 			setIdAudio(idDoAudio);
 		});
 	}
